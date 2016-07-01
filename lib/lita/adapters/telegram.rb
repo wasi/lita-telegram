6c4a8f6b76cdef96d7cb57add579d1b5c4523441
Philipp Wesner <pwesner@innomind.info>
2016-07-01 20:14:08 +0200

    add basic keyboard and options support

--- lib/lita/adapters/telegram.rb
+++ lib/lita/adapters/telegram.rb
@@ -30,11 +30,19 @@ module Lita
       def send_messages(target, messages)
         Thread.new do
           begin
+            opts = messages.pop if messages.last.is_a? Hash
             messages.each do |message|
               log.info "Outgoing Message: text=\"#{message}\" uid=#{target.room.to_i}"
               client.api.sendChatAction(chat_id: target.room.to_i, action: 'typing')
               sleep 2
-              client.api.sendMessage(chat_id: target.room.to_i, text: message)
+
+              if message == messages.last && opts
+                markup = ::Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: opts[:keyboard], one_time_keyboard: true)
+              else
+                markup = ::Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
+              end
+
+              client.api.sendMessage(chat_id: target.room.to_i, text: message, reply_markup: markup)
             end
           rescue => e
             log.error e.inspect
